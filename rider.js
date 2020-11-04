e7a47af79e2c6ea237dc6c4fc4fda10d4bb0cdbf
leo848 <leonardo.blume@gymnasium-essen-werden.de>
2020-11-04 16:29:52 +0100

    formatting

--- rider.js
+++ rider.js
@@ -1,57 +1,59 @@
 class Rider {
-    constructor() {
-        this.x = 50
-        this.xvel = 1
-        this.y = 200
-        this.yvel = 2
-        this.gravity = true
-        this.r = 20
-    }
+	constructor () {
+		this.x = 50;
+		this.xvel = 1;
+		this.y = 200;
+		this.yvel = 2;
+		this.gravity = true;
+		this.r = 20;
+	}
 
-    ride() {
+	ride () {
+		if (this.gravity) {
+			this.y += this.yvel;
+		} else {
+			this.y -= this.yvel;
+		}
 
-        if (this.gravity) {
-            this.y += this.yvel
-        } else {
-            this.y -= this.yvel
-        }
+		if (this.y >= height) {
+			this.y -= this.yvel + 1;
+		} else if (this.y <= 0) {
+			this.y += this.yvel + 1;
+		}
 
-        if (this.y >= height) {
-            this.y -= this.yvel + 1
-        } else if (this.y <= 0) {
-            this.y += this.yvel + 1
-        }
+		this.x += this.xvel;
+	}
 
-        this.x += this.xvel;
-    }
-
-    display() {
-        circle(this.x, this.y, this.r)
-    }
+	display () {
+		circle(this.x, this.y, this.r);
+	}
 }
 
 class Coin {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.r = 20;
-    }
+	constructor (x, y) {
+		this.x = x;
+		this.y = y;
+		this.r = 20;
+	}
 
-    display() {
-        push()
+	display () {
+		push();
 
-        stroke(0)
-        fill(255, 100, 0)
+		stroke(0);
+		fill(255, 100, 0);
 
-        circle(this.x, this.y, this.r)
+		circle(this.x, this.y, this.r);
 
-        pop()
-    }
+		pop();
+	}
 
-    plopIfRiderNearby() {
-        if (Math.abs(rider.x - this.x) < rider.r && Math.abs(rider.y - this.y) < rider.r) {
-            this.r = 0
-        }
-    }
-
+	plopIfRiderNearby () {
+		if (
+			Math.abs(rider.x - this.x) <
+				rider.r &&
+			Math.abs(rider.y - this.y) < rider.r
+		) {
+			this.r = 0;
+		}
+	}
 }
--- sketch.js
+++ sketch.js
@@ -2,38 +2,38 @@ let rider;
 let coins = [];
 let timer = 0;
 
-function setup() {
-    createCanvas(400, 400);
-    rider = new Rider()
-    for (let i = 0; i < 100; i++) {
-        coins.push(new Coin(i * random(50, 200) + 200, random(height)))
-    }
+function setup (){
+	createCanvas(400, 400);
+	rider = new Rider();
+	for (let i = 0; i < 100; i++) {
+		coins.push(
+			new Coin(
+				i * random(50, 200) + 200,
+				random(height),
+			),
+		);
+	}
 }
 
-function draw() {
+function draw (){
+	translate(-rider.xvel * frameCount, 0);
+	background(220, 120);
+	fill('orange');
+	noStroke();
 
-    translate(-rider.xvel * frameCount, 0)
-    background(220, 120)
-    fill("orange")
-    noStroke()
+	rider.ride();
+	rider.display();
 
+	for (let coin of coins) {
+		coin.display();
+		coin.plopIfRiderNearby();
+	}
 
-
-    rider.ride()
-    rider.display()
-
-    for (let coin of coins) {
-        coin.display()
-        coin.plopIfRiderNearby()
-    }
-
-    if (mouseIsPressed) {
-        rider.gravity = false
-    } else {
-        setTimeout(() => {
-            rider.gravity = true
-        }, 200)
-
-    }
-
+	if (mouseIsPressed) {
+		rider.gravity = false;
+	} else {
+		setTimeout(() => {
+			rider.gravity = true;
+		}, 200);
+	}
 }
